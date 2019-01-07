12bf327d86a4a71ad06d8ab52499f0146ddcace7
Parshant <parshant@ucreate.co.in>
2019-01-07 12:07:01 +0530

    adding functionality to fetch the all database in controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,13 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-	   $data = DB::select('SELECT datname FROM pg_database WHERE datistemplate = false');
-	   echo '<pre>'; print_r($data); die;
-	   //	    $response = \Terminal::command('sudo -u www-data ls')->execute();
-//	    echo '<pre>'; print_r($response); die;
-	    $output = array();
-	    //echo system("sudo su"); die;
-	    echo shell_exec("cd /var/www/html/; ./script.sh"); die;    
-	    return view('home');
+	   $database_list = DB::select('SELECT datname FROM pg_database WHERE datistemplate = false');   
+	   return view('home', compact('database_list'));
     }
 }
