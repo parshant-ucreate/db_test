b6b5fe7c9b3cebf6c7a4d3b8158c0616bd4551aa
Parshant <parshant@ucreate.co.in>
2019-01-07 15:12:42 +0530

    creating superuser and noraml user and assigning to database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,42 +24,52 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-	   $database_list = DB::select('select t1.datname AS name,  
-                                pg_size_pretty(pg_database_size(t1.datname)) as db_size
-                                from pg_database t1 WHERE datistemplate = false
-                                order by pg_database_size(t1.datname) desc;');   
+    public function index() {
+	   $database_list =   $this->fetchAllDatabaseListWithSize();
        return view('home', compact('database_list'));
     }
 
 
-    public function create_database()
-    {
+    protected function fetchAllDatabaseListWithSize() {
+        return DB::select('select t1.datname AS name,  
+                            pg_size_pretty(pg_database_size(t1.datname)) as db_size
+                            from pg_database t1 WHERE datistemplate = false
+                            order by pg_database_size(t1.datname) desc;'
+                        ); 
+    }
 
+    public function create_database() {
         if (request()->isMethod('post')) {
-            
             $validator = Validator::make(request()->all(), [
                 'name' => 'required|alpha_dash'
             ]);
-
             if ($validator->fails()) {
                 return Redirect::back()->withErrors($validator)->withInput();
             }
-
-            $name = strtolower(request()->name);
-
-            $response = DB::statement('CREATE DATABASE '.$name);
-
-            //dd($response);
+            $response = DB::statement('CREATE DATABASE '.strtolower(request()->name));
             if($response){
-                echo 'Success';
+                $d = $this->createDatabaseSuperUser(strtolower(request()->name));
+                $a = $this->createDatabaseNormalUser(strtolower(request()->name));
+                echo '<pre>'; print_r($d); echo '<br>'; echo '<pre>'; print_r($a); die;
                 die;
             }
-
         }
-   
        return view('create_database');
     }
 
+    protected function createDatabaseSuperUser($db_name) {
+        $database['user'] = str_random(10);
+        $database['password'] = str_random(25);
+        DB::select('create user '.$database['user'].' with encrypted password '.$database['password'].';'); 
+        DB::select('grant all privileges on database '.$db_name.'to '.$database['user'].';'); 
+        return $database;
+    }
+
+    protected function createDatabaseNormalUser($db_name) {
+        $database['user'] = str_random(10);
+        $database['password'] = str_random(25);
+        DB::select('create user '.$database['user'].' with encrypted password '.$database['password'].';'); 
+        DB::select('grant SELECT, INSERT, UPDATE privileges on database '.$db_name.'to '.$database['user'].';'); 
+        return $database;
+    }
 }
