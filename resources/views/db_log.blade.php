f7d182282b815fd59c3be4b95e4e69ef7de69c2c
Parshant <parshant@ucreate.co.in>
2019-01-10 16:51:14 +0530

    adding database log file

--- resources/views/db_log.blade.php
+++ resources/views/db_log.blade.php
@@ -1,11 +1,24 @@
-@extends('layouts.app')
+<?php
+$logFile = "/var/log/postgresql/postgresql-9.5-main.log"; // local path to log file
+$interval = 1000; //how often it checks the log file for changes, min 100
+$textColor = ""; //use CSS color
 
-@section('content')
-<style>
+
+
+// Don't have to change anything bellow
+if(!$textColor) $textColor = "white";
+if($interval < 100)  $interval = 100; 
+if(isset($_GET['getLog'])){
+  echo file_get_contents($logFile);
+}else{
+?>
+<html>
+  <title>Log</title>
+  <style>
     @import url(http://fonts.googleapis.com/css?family=Ubuntu);
     body{
       background-color: black;
-      color: white;
+      color: <?php echo $textColor; ?>;
       font-family: 'Ubuntu', sans-serif;
       font-size: 16px;
       line-height: 20px;  
@@ -25,47 +38,41 @@
       overflow:visible;
     }
   </style>
-<div class="container">
-    <div class="row justify-content-center">
-       @php $interval = 1000; $textColor = "white"; @endphp
-       @if($interval < 100)  $interval = 100; @endif
-          @if(isset($log_file_url))
-            {{file_get_contents($log_file_url)}}
-          @else
-            <script>
-              setInterval(readLogFile, <?php echo $interval; ?>);
-              window.onload = readLogFile; 
-              var pathname = window.location.pathname;
-              var scrollLock = true;
-              
-              $(document).ready(function(){
-                $('.disableScrollLock').click(function(){
-                  $("html,body").clearQueue()
-                  $(".disableScrollLock").hide();
-                  $(".enableScrollLock").show();
-                  scrollLock = false;
-                });
-                $('.enableScrollLock').click(function(){
-                  $("html,body").clearQueue()
-                  $(".enableScrollLock").hide();
-                  $(".disableScrollLock").show();
-                  scrollLock = true;
-                });
-              });
-              function readLogFile(){
-                $.get(pathname, { getLog : "true" }, function(data) {
-                  data = data.replace(new RegExp("\n", "g"), "<br />");
-                      $("#log").html(data);
-                      if(scrollLock == true) { $('html,body').animate({scrollTop: $("#scrollLock").offset().top}, <?php echo $interval; ?>) };
-                  });
-              }
-            </script>
-              <h4><?php echo $log_file_url; ?></h4>
-              <div id="log">
-                
-              </div>
-              <div id="scrollLock"> <input class="disableScrollLock" type="button" value="Disable Scroll Lock" /> <input class="enableScrollLock" style="display: none;" type="button" value="Enable Scroll Lock" /></div>
-            @endif  
-</div>
-</div>
-@endsection
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
+  <script>
+    setInterval(readLogFile, <?php echo $interval; ?>);
+    window.onload = readLogFile; 
+    var pathname = window.location.pathname;
+    var scrollLock = true;
+    
+    $(document).ready(function(){
+      $('.disableScrollLock').click(function(){
+        $("html,body").clearQueue()
+        $(".disableScrollLock").hide();
+        $(".enableScrollLock").show();
+        scrollLock = false;
+      });
+      $('.enableScrollLock').click(function(){
+        $("html,body").clearQueue()
+        $(".enableScrollLock").hide();
+        $(".disableScrollLock").show();
+        scrollLock = true;
+      });
+    });
+    function readLogFile(){
+      $.get(pathname, { getLog : "true" }, function(data) {
+        data = data.replace(new RegExp("\n", "g"), "<br />");
+            $("#log").html(data);
+            if(scrollLock == true) { $('html,body').animate({scrollTop: $("#scrollLock").offset().top}, <?php echo $interval; ?>) };
+        });
+    }
+  </script>
+  <body>
+    <h4><?php echo $logFile; ?></h4>
+    <div id="log">
+      
+    </div>
+    <div id="scrollLock"> <input class="disableScrollLock" type="button" value="Disable Scroll Lock" /> <input class="enableScrollLock" style="display: none;" type="button" value="Enable Scroll Lock" /></div>
+  </body>
+</html>
+<?php  } ?>
