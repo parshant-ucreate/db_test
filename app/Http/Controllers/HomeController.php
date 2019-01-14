1472232d7cfec958d9bb6834228a14552e41a879
Vikramjit Singh <vikramjitsingh@ucreate.co.in>
2019-01-14 17:54:58 +0530

    queue migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -299,4 +299,30 @@ class HomeController extends Controller
         }
         return view('import_database', compact('db_name','success'));
     }
+
+     protected function backupDatabaseCron() {
+        if (!is_dir('db_backup/')) {
+            mkdir('db_backup');         
+        }
+
+        $database_list = DbList::get()->toArray();
+
+        foreach ($database_list as $key => $db) {
+            $fileName = $db['name'].'_'.time().'.sql';
+            exec('pg_dump --dbname=postgresql://'.getenv('DB_USERNAME').':'.getenv('DB_PASSWORD').'@'.getenv('DB_HOST').':'.getenv('DB_PORT').'/'.$db['name'].' > db_backup/'.$fileName .' 2>&1' ,$output);
+             
+            //save in to local database
+            DbBackup::create(['filename' => $fileName, 'database_list_id' => $db['id'], 'type' => 'auto']);
+
+            //Move to S3
+            $s3 = Storage::disk('s3');
+            $filePath = $db['name'].'/' . $fileName;
+            $res = $s3->put($filePath, file_get_contents('db_backup/'.$fileName));
+
+            //remove file from server
+            unlink('db_backup/'.$fileName);
+        }
+        dd('complete');
+    }
+
 }
