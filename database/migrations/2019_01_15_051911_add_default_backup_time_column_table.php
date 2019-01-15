668d0bc2999a7b775719582f2d3e8e27965bb984
Vikramjit Singh <vikramjitsingh@ucreate.co.in>
2019-01-15 11:35:58 +0530

    add default backup_time column to database list table migration

---
+++ database/migrations/2019_01_15_051911_add_default_backup_time_column_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDefaultBackupTimeColumnTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('database_list', function (Blueprint $table) {
+            $table->integer('backp_time')->default(env('BACKUP_DEFAULT_TIME',240))->comment('time in minutes like 5 or 60'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('database_list', function (Blueprint $table) {
+            $table->dropColumn('backp_time');  
+        });
+    }
+}
